fcc0bfb86a0b692a630b98d5308fc70b88574c3a
0x5b62656e5d <salt@pepper.fyi>
2025-08-31 11:28:41 +0800

    Create method to create tables with generic types

--- src/list_files.rs
+++ src/list_files.rs
@@ -1,10 +1,10 @@
-use crate::util::round;
+use crate::util::{build_table, round};
 use aws_sdk_s3::Client;
 use aws_sdk_s3::operation::list_objects_v2::ListObjectsV2Output;
 use aws_sdk_s3::types::Object;
 use byte_unit::{Byte, UnitType};
 use chrono::prelude::*;
-use tabled::{Table, Tabled, settings::Style};
+use tabled::{Table, Tabled};
 
 #[derive(Tabled)]
 struct FileInfo {
@@ -16,33 +16,24 @@ struct FileInfo {
 pub async fn list_files(client: &Client, bucket: &str) -> Result<Table, anyhow::Error> {
     let res: ListObjectsV2Output = client.list_objects_v2().bucket(bucket).send().await?;
 
-    let mut table: Table = Table::new(
-        res.contents
+    let table: Table = build_table(res.contents.unwrap(), |o: &Object| {
+        let size = Byte::from_i64(o.size().unwrap_or_else(|| 0))
             .unwrap()
-            .iter()
-            .map(|o: &Object| {
-                let size = Byte::from_i64(o.size().unwrap_or_else(|| 0))
-                    .unwrap()
-                    .get_appropriate_unit(UnitType::Decimal);
+            .get_appropriate_unit(UnitType::Decimal);
 
-                let timestamp = DateTime::from_timestamp_millis(
-                    o.last_modified().unwrap().to_millis().unwrap(),
-                )
+        let timestamp =
+            DateTime::from_timestamp_millis(o.last_modified().unwrap().to_millis().unwrap())
                 .unwrap()
                 .with_timezone(&Local)
                 .format("%b %d, %Y - %H:%M:%S")
                 .to_string();
 
-                FileInfo {
-                    key: o.key().unwrap().to_string(),
-                    last_modified: timestamp,
-                    size: format!("{:?} {:?}", round(size.get_value(), 2), size.get_unit()),
-                }
-            })
-            .collect::<Vec<FileInfo>>(),
-    );
-
-    table.with(Style::modern());
+        FileInfo {
+            key: o.key().unwrap().to_string(),
+            last_modified: timestamp,
+            size: format!("{:?} {:?}", round(size.get_value(), 2), size.get_unit()),
+        }
+    });
 
     Ok(table)
 }
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,26 @@
+use tabled::{
+    Table, Tabled,
+    settings::{Color, Modify, Style, object::Rows},
+};
+
 pub fn round(value: f64, precision: u32) -> f64 {
     let factor: f64 = 10f64.powi(precision as i32);
 
     (value * factor).round() / factor
+}
+
+pub fn build_table<A, B, C, D>(data: A, map_fn: B) -> Table
+where
+    A: IntoIterator<Item = C>,
+    B: Fn(&C) -> D,
+    D: Tabled,
+{
+    Table::new(
+        data.into_iter()
+            .map(|item: C| map_fn(&item))
+            .collect::<Vec<D>>(),
+    )
+    .with(Style::modern())
+    .with(Modify::new(Rows::first()).with(Color::FG_BRIGHT_MAGENTA))
+    .to_owned()
 }
